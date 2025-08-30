22a63ff074585ae06cb97ca34f9e6c842cb23261
priyanshh-09 <priyanshuchandola9@gmail.com>
2025-08-30 18:07:48 +0530

    make add button interactive using form constructor

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,22 +1,49 @@
 
 
-export default function Expenseform() {
+export default function Expenseform({setExpenses}) {
+    const handleSubmit = (e)=>{
+      e.preventDefault()
+      setExpenses((prevState) => [
+        ... prevState, 
+        {...getFormData(e.target), id: crypto.randomUUID}]) }
+
+    const getFormData = (form)=>{
+          const Formdata = new FormData(form);
+          const data = {};
+
+          for (const [key, value] of Formdata.entries()) {
+            data[key] = value;
+          }
+
+         return data;
+    }
+
   return (
-    <htmlform className="expense-htmlform">
+
+    <form className="expense-form" onSubmit={handleSubmit}>
       <div className="input-container">
         <label htmlfor="title">Title</label>
-        <input id="title" />
+        <input id="title"  name="title"/>
       </div>
+
       <div className="input-container">
+
         <label htmlfor="category">Category</label>
-        <input id="category" />
+
+        <select id="category" name="category">
+          <option hidden>Select Category</option>
+          <option value="grocery">Grocery</option>
+          <option value="clothes">Clothes</option>
+          <option value="bills">Bills</option>
+          <option value="education">Education</option>
+          <option value="medicine">Medicine</option>
+        </select>
       </div>
       <div className="input-container">
         <label htmlfor="amount">Amount</label>
-        <input id="amount" />
+        <input id="amount" name="amount"/>
       </div>
       <button className="add-btn">Add</button>
-    </htmlform>
- 
+    </form>
   );
 }
