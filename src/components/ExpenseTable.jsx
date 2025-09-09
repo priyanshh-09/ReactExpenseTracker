052a70b962b87b14c4e195f70d176278b77d2c4b
priyanshh-09 <priyanshuchandola9@gmail.com>
2025-09-09 12:23:37 +0530

    Implement the Sorting functionality in table row

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -21,11 +21,21 @@ export default function ExpenseTable({expenses, setExpenses, setExpense, setEdit
   
   return (
     <>
-    <ContextMenu menuPosition={menuPosition} setExpense={setExpense} setMenuPosition={setMenuPosition} setExpenses={setExpenses} rowid={rowid} expenses={expenses}
-      setEditingRowId={setEditingRowId}
-    />
+      <ContextMenu
+        menuPosition={menuPosition}
+        setExpense={setExpense}
+        setMenuPosition={setMenuPosition}
+        setExpenses={setExpenses}
+        rowid={rowid}
+        expenses={expenses}
+        setEditingRowId={setEditingRowId}
+      />
 
-      <table className="expense-table" onClick={()=> setMenuPosition({})}>
+      <table className="expense-table" onClick={() =>{
+        if(menuPosition.left){
+          setMenuPosition({});
+        }
+      }}>
         <thead>
           <tr>
             <th>Title</th>
@@ -47,6 +57,11 @@ export default function ExpenseTable({expenses, setExpenses, setExpense, setEdit
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
+                  onClick={() => {
+                    setExpenses((prevState) => [
+                       ... prevState.sort((a, b) => a.amount - b.amount)],
+                    );
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -56,6 +71,12 @@ export default function ExpenseTable({expenses, setExpenses, setExpense, setEdit
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setExpenses((prevState) => [
+                      ...prevState.sort((a, b) => b.amount - a.amount),
+                    ]);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -66,14 +87,15 @@ export default function ExpenseTable({expenses, setExpenses, setExpense, setEdit
         </thead>
         <tbody>
           {FilteredData.map(({ id, title, category, amount }) => {
-
             return (
-              <tr key={id} onContextMenu={(e) => {
-                   e.preventDefault();
-                   setMenuPosition({left:e.clientX + 4,top: e.clientY + 4});
-                   setRowId(id);
-                } 
-              }>
+              <tr
+                key={id}
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  setMenuPosition({ left: e.clientX + 4, top: e.clientY + 4 });
+                  setRowId(id);
+                }}
+              >
                 <td>{title}</td>
                 <td>{category}</td>
                 <td>₹{amount}</td>
